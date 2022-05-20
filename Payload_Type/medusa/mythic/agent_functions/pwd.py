18553c589038eedbd071ceb4d1dadc605b414c8a
mihaid-b <90832132+mihaid-b@users.noreply.github.com>
2022-05-20 18:46:15 +0300

    Added pwd and whoami agen functions.

---
+++ Payload_Type/medusa/mythic/agent_functions/pwd.py
@@ -0,0 +1,39 @@
+from mythic_payloadtype_container.MythicCommandBase import *
+import json
+from mythic_payloadtype_container.MythicRPC import *
+
+
+class GetPwdArguments(TaskArguments):
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line, **kwargs)
+        self.args = []
+
+    async def parse_arguments(self):
+        pass
+
+
+class GetPwdCommand(CommandBase):
+    cmd = "pwd"
+    needs_admin = False
+    help_cmd = "pwd"
+    description = "This prins the current working directory"
+    version = 1
+    is_exit = False
+    is_file_browse = False
+    is_process_list = False
+    is_download_file = False
+    is_remove_file = False
+    is_upload_file = False
+    author = "@mihaid-b"
+    argument_class = GetPwdArguments
+    attackmapping = []
+    attributes = CommandAttributes(
+        supported_python_versions=["Python 2.7", "Python 3.8"],
+        supported_os=[SupportedOS.MacOS, SupportedOS.Windows, SupportedOS.Linux],
+    )
+
+    async def create_tasking(self, task: MythicTask) -> MythicTask:
+        return task
+
+    async def process_response(self, response: AgentResponse):
+        pass
---
+++ Payload_Type/medusa/mythic/agent_functions/whoami.py
@@ -0,0 +1,25 @@
+from mythic_payloadtype_container.MythicCommandBase import *
+
+class WhoamiArguments(TaskArguments):
+    def __init__(self, command_line):
+        super().__init__(command_line)
+        self.args = {}
+
+    async def parse_arguments(self):
+        pass
+
+class WhoamiCommand(CommandBase):
+    cmd = "whoami"
+    needs_admin = False
+    help_cmd = "whoami"
+    description = "Prints the effective username for the agent"
+    version = 1
+    author = "@mihaid-b"
+    attackmapping = ["T1033"]
+    argument_class = WhoamiArguments
+
+    async def create_tasking(self, task: MythicTask) -> MythicTask:
+        return task
+
+    async def process_response(self, response: AgentResponse):
+        pass
